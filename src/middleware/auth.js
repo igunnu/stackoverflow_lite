8265f6d5ad0d215c76399cb14cf170e87d94cd03
igunnu <emmanuel.igunnu@flutterwavego.com>
2022-09-16 10:52:38 +0100

    Added  middleware for protected routes

---
+++ src/middleware/auth.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+const asyncHandler = require('./async');
+const ErrorResponse = require('../utils/errorResponse');
+const db = require('../models/index');
+
+const User = db.users;
+
+// eslint-disable-next-line consistent-return
+exports.protectedRoute = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    // eslint-disable-next-line prefer-destructuring
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    return next(new ErrorResponse('Login required', 401));
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findByPk(decoded.id);
+    next();
+  } catch (err) {
+    return next(new ErrorResponse('Invalid Bearer Token', 401));
+  }
+});

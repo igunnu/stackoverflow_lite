e39b7d13854fe6c4fdaa860e79a70c1e42314904
igunnu <emmanuel.igunnu@flutterwavego.com>
2022-09-19 16:01:27 +0100

    Feat: user can post comment

--- src/controllers/answer.js
+++ src/controllers/answer.js
@@ -2,7 +2,7 @@ const db = require('../models/index');
 const asyncHandler = require('../middleware/async');
 const ErrorResponse = require('../utils/errorResponse');
 
-const User = db.users;
+const Comment = db.comments;
 const Question = db.questions;
 const Answer = db.answers;
 const Vote = db.votes;
@@ -59,3 +59,24 @@ exports.vote = asyncHandler(async (req, res, next) => {
     return next(new ErrorResponse('internal server error', 500));
   }
 });
+
+exports.postComment = asyncHandler(async (req, res, next) => {
+  const { body } = req.body;
+  if (!body) {
+    return next(new ErrorResponse('body is required', 400));
+  }
+
+  const data = {
+    body,
+    userId: req.user.id,
+    answerId: req.params.answerd
+  };
+
+  await Comment.create(data);
+  return res.status(201).json({
+    status: 'success',
+    data: {
+      message: 'Comment submitted'
+    }
+  });
+})

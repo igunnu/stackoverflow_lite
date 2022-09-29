5cd078a35eec9c120bd3bd6f7cb6582de19d07d4
igunnu <emmanuel.igunnu@flutterwavego.com>
2022-09-29 14:41:35 +0100

    feat: user can get top question

--- src/controllers/questions.js
+++ src/controllers/questions.js
@@ -185,3 +185,35 @@ exports.myQuestions = asyncHandler(async (req, res, next) => {
     }
   });
 });
+
+exports.mostAnsweredQuestion = asyncHandler(async (req, res, next) => {
+  const [question] = await sequelize.query(`SELECT 
+                                            questions.id, 
+                                            questions.title, 
+                                            questions.body, 
+                                            questions.acceptedAnswer, 
+                                            questions.createdAt, 
+                                            questions.updatedAt, 
+                                            users.username as author 
+                                          FROM questions 
+                                          JOIN 
+                                            answers 
+                                            ON 
+                                              answers.questionId = questions.id 
+                                          JOIN users 
+                                            ON 
+                                              questions.userId = users.id 
+                                          GROUP BY 
+                                            questions.id 
+                                          ORDER BY 
+                                            COUNT(answers.id) 
+                                          DESC
+                                          LIMIT 1;`);
+  return res.status(200).json({
+    status: 'success',
+    data: {
+      question,
+      message: 'success'
+    }
+  });
+});
